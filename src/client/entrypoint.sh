15d364fd1a28bd22df4eb67fa6c84141e9437e9e
Aman Agarwal <aman@appsmith.com>
2024-02-23 03:47:49 +0530

    feat: update prisma datsource url

--- src/client/entrypoint.sh
+++ src/client/entrypoint.sh
@@ -8,7 +8,7 @@ export NEXTAUTH_SECRET=$(openssl rand -base64 32)
 echo "NEXTAUTH_SECRET=$NEXTAUTH_SECRET" >> /etc/environment
 echo "PORT=${PORT:-3000}" >> /etc/environment
 echo "NEXTAUTH_URL=http://localhost:$PORT" >> /etc/environment
-echo "DATABASE_URL=$DATABASE_URL" >> /etc/environment
+echo "DATABASE_URL=${DATABASE_URL:-file:../.db2/data.db}" >> /etc/environment
 
 # Run Prisma migrations and generate prisma client
 npx prisma migrate deploy
