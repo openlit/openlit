46de3914f013148d1a365c46126a212826ca6e6d
Aman Agarwal <aman@appsmith.com>
2024-03-02 03:12:33 +0530

    feat: updated homepage

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -2,8 +2,6 @@ import Features from "@/components/(landing-page)/components/features";
 import Hero from "@/components/(landing-page)/components/hero";
 import Navbar from "@/components/(landing-page)/components/navbar";
 import WhyDoku from "@/components/(landing-page)/components/whydoku";
-import { ArrowRightIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
 
 export default function Home() {
 	return (
