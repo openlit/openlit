ef407ca7d7ded14ea7e78293daa5f17fb2f4fe55
Aman Agarwal <agarwal.aman041@gmail.com>
2024-03-05 02:14:27 +0530

    feat: homepage revamp (#86)

--- src/client/src/app/layout.tsx
+++ src/client/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" className="scroll-smooth">
 			<body className={inter.className}>
 				{children}
 				<Toaster
