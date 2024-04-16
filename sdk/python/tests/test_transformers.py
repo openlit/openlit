03431326fcea1a1db426d0b61db89243d3f0f476
patcher9 <patcher99@dokulabs.com>
2024-04-16 13:09:33 +0530

    pylint

--- sdk/python/tests/test_transformers.py
+++ sdk/python/tests/test_transformers.py
@@ -1,14 +1,11 @@
 # pylint: disable=duplicate-code, no-name-in-module
 """
-This module contains tests for Anthropic functionality using the Anthropic Python library.
+This module contains tests for Text Generation functionality in HuggingFace Transformers library.
 
-Tests cover various API endpoints, including chat. 
+Tests cover the usage of the Transformers' pipeline for generating text. 
 These tests validate integration with OpenLIT.
 
-Environment Variables:
-    - ANTHROPIC_API_TOKEN: Anthropic API api_key for authentication.
-
-Note: Ensure the environment is properly configured for Anthropic access and OpenLIT monitoring
+Note: Ensure the environment is properly configured for Transformers and OpenLIT monitoring
 prior to running these tests.
 """
 
@@ -18,12 +15,16 @@ import openlit
 # Initialize environment and application name for OpenLIT monitoring
 openlit.init(environment="openlit-testing", application_name="openlit-python-test")
 
-def test_sync_anthropic_messages():
+def test_text_trasnformers():
     """
-    Tests synchronous messages with the 'claude-3-haiku-20240307' model.
+    Test text generation capabilities using the GPT-2 model from HuggingFace Transformers library.
+
+    This test sends a prompt to a pre-specified model and verifies that the response contains
+    generated text matching expected criteria. 
+    In this case, simply the presence of the 'generated_text' field in the response object.
 
     Raises:
-        AssertionError: If the messages response object is not as expected.
+        AssertionError: If the 'generated_text' field is missing from any part of the response.
     """
 
     generator = pipeline(model="openai-community/gpt2")
