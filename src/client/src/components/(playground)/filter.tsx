b539bf6079ba4cbea7182f2f50554bb1dfaf1284
Aman Agarwal <agarwal.aman041@gmail.com>
2024-05-10 13:42:28 +0530

    feat: support vector display on table and details popover, custom date range (#218)
    Co-authored-by: patcher9 <patcher99@dokulabs.com>

--- src/client/src/components/(playground)/filter.tsx
+++ src/client/src/components/(playground)/filter.tsx
@@ -1,14 +1,16 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getFilterDetails, getUpdateFilter } from "@/selectors/filter";
 import { useRootStore } from "@/store";
+import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 
 const DEFAULT_TIME_RANGE = "24H";
 
-const TIME_RANGE_TYPE: Record<"24H" | "7D" | "1M" | "3M", string> = {
+const TIME_RANGE_TYPE: Record<"24H" | "7D" | "1M" | "3M" | "CUSTOM", string> = {
 	"24H": "24H",
 	"7D": "7D",
 	"1M": "1M",
 	"3M": "3M",
+	CUSTOM: "CUSTOM",
 };
 
 const TIME_RANGE_TABS: { key: string; label: string }[] = Object.keys(
@@ -25,20 +27,28 @@ const Filter = () => {
 		updateFilter("timeLimit.type", key);
 	};
 
+	const onCustomDateChange = (start: Date, end: Date) => {
+		updateFilter("timeLimit.type", TIME_RANGE_TYPE.CUSTOM, { start, end });
+	};
+
 	return (
-		<Tabs
-			defaultValue={DEFAULT_TIME_RANGE}
-			className="w-[400px] mb-4 md:mb-4"
-			onValueChange={handleChange}
-		>
-			<TabsList>
-				{TIME_RANGE_TABS.map(({ label, key }) => (
-					<TabsTrigger key={key} value={key}>
-						{label}
-					</TabsTrigger>
-				))}
-			</TabsList>
-		</Tabs>
+		<div className="flex w-full mb-4 md:mb-4 gap-4">
+			<Tabs defaultValue={DEFAULT_TIME_RANGE} onValueChange={handleChange}>
+				<TabsList>
+					{TIME_RANGE_TABS.map(({ label, key }) => (
+						<TabsTrigger key={key} value={key}>
+							{label}
+						</TabsTrigger>
+					))}
+				</TabsList>
+			</Tabs>
+			{filter.timeLimit.type === "CUSTOM" && (
+				<DatePickerWithRange
+					selectedDate={filter.timeLimit}
+					onCustomDateChange={onCustomDateChange}
+				/>
+			)}
+		</div>
 	);
 };
 
---
+++ src/client/src/components/ui/date-range-picker.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import * as React from "react";
+import { addDays, format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePickerWithRange({
+	className,
+	onCustomDateChange,
+	selectedDate,
+}: React.HTMLAttributes<HTMLDivElement> & {
+	onCustomDateChange: (start: Date, end: Date) => void;
+	selectedDate?: {
+		start?: Date;
+		end?: Date;
+	};
+}) {
+	const [date, setDate] = React.useState<DateRange | undefined>({
+		from: selectedDate?.start || addDays(new Date(), -15),
+		to: selectedDate?.end || new Date(),
+	});
+
+	const onCustomChange = React.useCallback(() => {
+		if (date?.from && date?.to) onCustomDateChange(date?.from, date?.to);
+	}, [date]);
+
+	return (
+		<div className={cn("grid gap-2", className)}>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button
+						id="date"
+						variant={"outline"}
+						className={cn(
+							"justify-start text-left font-normal dark:bg-stone-900 dark:text-stone-100",
+							!date && "text-muted-foreground"
+						)}
+					>
+						<CalendarIcon className="mr-2 h-4 w-4" />
+						{date?.from ? (
+							date.to ? (
+								<>
+									{format(date.from, "LLL dd, y")} -{" "}
+									{format(date.to, "LLL dd, y")}
+								</>
+							) : (
+								format(date.from, "LLL dd, y")
+							)
+						) : (
+							<span>Pick a date</span>
+						)}
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="w-auto p-0" align="start">
+					<Calendar
+						initialFocus
+						mode="range"
+						defaultMonth={date?.from}
+						selected={date}
+						onSelect={setDate}
+						numberOfMonths={2}
+					/>
+					<div className="flex justify-end px-3 pb-3">
+						<Button
+							className="w-20"
+							variant="default"
+							size="xs"
+							disabled={!date?.from || !date?.to}
+							onClick={onCustomChange}
+						>
+							Done
+						</Button>
+					</div>
+				</PopoverContent>
+			</Popover>
+		</div>
+	);
+}
