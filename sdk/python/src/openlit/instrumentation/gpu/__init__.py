5cc7ee60d3e8ddf97102e085c8132cbd84fc5ea3
patcher9 <patcher99@dokulabs.com>
2024-06-06 15:08:37 +0530

    update try and expect error

--- sdk/python/src/openlit/instrumentation/gpu/__init__.py
+++ sdk/python/src/openlit/instrumentation/gpu/__init__.py
@@ -4,14 +4,14 @@
 from typing import Dict, Collection
 import time
 import schedule
-from opentelemetry import metrics
+import logging
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from functools import partial
 from opentelemetry.sdk.resources import TELEMETRY_SDK_NAME
 from openlit.semcov import SemanticConvetion
 
-# Importing module 
-import importlib 
+# Initialize logger for logging potential issues and operations
+logger = logging.getLogger(__name__)
 
 class NvidiaGPUInstrumentor(BaseInstrumentor):
     """
@@ -51,38 +51,42 @@ def start_schedule(metrics_dict, gpu_stats_interval, environment, application_na
     scheduler_thread.start()
 
 def collect_metrics(metrics_dict, environment, application_name):
-    import gpustat
-    
-    """Collect metrics from GPU statistics"""
-    gpu_stats = gpustat.GPUStatCollection.new_query()
+    try:
+        import gpustat
+        
+        """Collect metrics from GPU statistics"""
+        gpu_stats = gpustat.GPUStatCollection.new_query()
 
-    for gpu in gpu_stats.gpus:
-        attributes = {
-            TELEMETRY_SDK_NAME:
-                "openlit",
-            SemanticConvetion.GEN_AI_APPLICATION_NAME:
-                application_name,
-            SemanticConvetion.GEN_AI_SYSTEM:
-                SemanticConvetion.GEN_AI_SYSTEM_MISTRAL,
-            SemanticConvetion.GEN_AI_ENVIRONMENT:
-                environment,
-            SemanticConvetion.GPU_INDEX:
-                gpu.index,
-            SemanticConvetion.GPU_UUID:
-                gpu.uuid,
-            SemanticConvetion.GPU_NAME:
-                gpu.name,
+        for gpu in gpu_stats.gpus:
+            attributes = {
+                TELEMETRY_SDK_NAME:
+                    "openlit",
+                SemanticConvetion.GEN_AI_APPLICATION_NAME:
+                    application_name,
+                SemanticConvetion.GEN_AI_SYSTEM:
+                    SemanticConvetion.GEN_AI_SYSTEM_MISTRAL,
+                SemanticConvetion.GEN_AI_ENVIRONMENT:
+                    environment,
+                SemanticConvetion.GPU_INDEX:
+                    gpu.index,
+                SemanticConvetion.GPU_UUID:
+                    gpu.uuid,
+                SemanticConvetion.GPU_NAME:
+                    gpu.name,
 
-        }
+            }
 
-        metrics_dict["gpu_utilization"].record(gpu.utilization if gpu.utilization else 0, attributes)
-        metrics_dict["gpu_utilization_enc"].record(gpu.utilization_enc if gpu.utilization_enc else 0, attributes)
-        metrics_dict["gpu_utilization_dec"].record(gpu.utilization_dec if gpu.utilization_dec else 0, attributes)
-        metrics_dict["gpu_temperature"].record(gpu.temperature if gpu.temperature else 0, attributes)
-        metrics_dict["gpu_fan_speed"].record(gpu.fan_speed if gpu.fan_speed else 0, attributes)
-        metrics_dict["gpu_memory_available"].record(gpu.memory_available if gpu.memory_available else 0, attributes)
-        metrics_dict["gpu_memory_total"].record(gpu.memory_total if gpu.memory_total else 0, attributes)
-        metrics_dict["gpu_memory_used"].record(gpu.memory_used if gpu.memory_used else 0, attributes)
-        metrics_dict["gpu_memory_free"].record(gpu.memory_free if gpu.memory_free else 0, attributes)
-        metrics_dict["gpu_power_draw"].record(gpu.power_draw if gpu.power_draw else 0, attributes)
-        metrics_dict["gpu_power_limit"].record(gpu.power_limit if gpu.power_limit else 0, attributes)
+            metrics_dict["gpu_utilization"].record(gpu.utilization if gpu.utilization else 0, attributes)
+            metrics_dict["gpu_utilization_enc"].record(gpu.utilization_enc if gpu.utilization_enc else 0, attributes)
+            metrics_dict["gpu_utilization_dec"].record(gpu.utilization_dec if gpu.utilization_dec else 0, attributes)
+            metrics_dict["gpu_temperature"].record(gpu.temperature if gpu.temperature else 0, attributes)
+            metrics_dict["gpu_fan_speed"].record(gpu.fan_speed if gpu.fan_speed else 0, attributes)
+            metrics_dict["gpu_memory_available"].record(gpu.memory_available if gpu.memory_available else 0, attributes)
+            metrics_dict["gpu_memory_total"].record(gpu.memory_total if gpu.memory_total else 0, attributes)
+            metrics_dict["gpu_memory_used"].record(gpu.memory_used if gpu.memory_used else 0, attributes)
+            metrics_dict["gpu_memory_free"].record(gpu.memory_free if gpu.memory_free else 0, attributes)
+            metrics_dict["gpu_power_draw"].record(gpu.power_draw if gpu.power_draw else 0, attributes)
+            metrics_dict["gpu_power_limit"].record(gpu.power_limit if gpu.power_limit else 0, attributes)
+        
+    except Exception as e:
+        logger.error("Error in GPU metrics collection: %s", e)
