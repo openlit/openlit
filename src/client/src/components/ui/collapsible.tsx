cb25ce7b50b5b4e5e2f9c61c8aa60424a28320c3
Aman Agarwal <aman@appsmith.com>
2024-06-29 14:43:11 +0530

    fix: docker image failure

--- src/client/src/components/ui/collapsible.tsx
+++ src/client/src/components/ui/collapsible.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { cn } from '@/lib/utils';
-import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
-import * as React from 'react';
+import { cn } from "@/lib/utils";
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import * as React from "react";
 
 const Collapsible = CollapsiblePrimitive.Root;
 
@@ -14,9 +14,14 @@ const CollapsibleContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<CollapsiblePrimitive.CollapsibleContent
 		ref={ref}
-		className={cn('overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up', className)}
+		className={cn(
+			"overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up",
+			className
+		)}
 		{...props}
 	/>
 ));
+
+CollapsibleContent.displayName = "CollapsibleContent";
 
 export { Collapsible, CollapsibleContent, CollapsibleTrigger };
