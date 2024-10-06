f92276be4d4979ee615251d2a2b6fcc7f112f477
Patcher <patcher@openlit.io>
2024-10-06 18:34:26 +0530

    Update test_milvus.py

--- sdk/python/tests/test_milvus.py
+++ sdk/python/tests/test_milvus.py
@@ -27,7 +27,7 @@ client = MilvusClient(
 # Initialize environment and application name for OpenLIT monitoring
 openlit.init(environment="openlit-testing", application_name="openlit-python-test")
 
-COLLECTION_NAME = "openlit"
+COLLECTION_NAME = "OpenLIT"
 
 def test_db_milvus():
     """
