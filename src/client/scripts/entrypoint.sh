34f7dcc81fad8d7de35cdae4715c971ee61cb9f3
Aman Agarwal <aman@appsmith.com>
2024-11-15 03:02:54 +0530

    fix: posthog secrets in the docker file

--- src/client/scripts/entrypoint.sh
+++ src/client/scripts/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Run fetching posthog creds
-set +e
-./fetch-posthog-creds.sh
-set -e
-
 # Generate and set NextAuth.js secret as an environment variable
 export NEXTAUTH_SECRET=$(openssl rand -base64 32)
 
 # Set NextAuth.js environment variables
 # Default Telemetry enabled, Set this to false to disable telemetry tracking
 echo "TELEMETRY_ENABLED=true" >> /etc/environment
+
+# POSTHOG Key
+echo "POSTHOG_API_KEY=phc_JiR0FxzxAYeV3gDoMUltOxjLa3r7RnogMHPVrjDzTR9" >> /etc/environment
+echo "POSTHOG_HOST=https://us.i.posthog.com" >> /etc/environment
 
 echo "NEXTAUTH_SECRET=$NEXTAUTH_SECRET" >> /etc/environment
 echo "NEXTAUTH_URL=http://localhost:3000" >> /etc/environment
