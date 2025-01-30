341284a48fd835856b9f9bbeafe321f9626295f2
Ustatleen Kaur <ust.kaur@gmail.com>
2025-01-30 14:19:41 +0530

    feat: removed lodash

--- src/client/src/middleware/check-demo-account.ts
+++ src/client/src/middleware/check-demo-account.ts
@@ -2,7 +2,6 @@ import { NextFetchEvent, NextMiddleware, NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { RESTRICTED_DEMO_ACCOUNT_ROUTES } from '@/constants/route';
 import { getToken } from 'next-auth/jwt';
-import { isEmpty } from 'lodash';
 
 
 export default function checkDemoAccount(next: NextMiddleware) {
@@ -10,7 +9,7 @@ export default function checkDemoAccount(next: NextMiddleware) {
     request: NextRequest,
     _next: NextFetchEvent
   ) => {
-    if (!isEmpty(process.env.DEMO_ACCOUNTS ?? "")) {
+    if (process.env.DEMO_ACCOUNTS && process.env.DEMO_ACCOUNTS !== "") {
       const pathname = request.nextUrl.pathname;
       if (RESTRICTED_DEMO_ACCOUNT_ROUTES[request.method]?.some((regex) => (new RegExp(regex)).test(pathname))) {
         const token = await getToken({ req: request });
