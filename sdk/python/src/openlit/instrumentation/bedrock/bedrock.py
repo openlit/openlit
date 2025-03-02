80867f9c40c6200d4b73d7c67fa62deb592f7a31
patcher9 <patcher@openlit.io>
2025-03-02 18:02:39 +0530

    test

--- sdk/python/src/openlit/instrumentation/bedrock/bedrock.py
+++ sdk/python/src/openlit/instrumentation/bedrock/bedrock.py
@@ -116,7 +116,7 @@ def converse(version, environment, application_name, tracer,
                         content = message['content']
 
                         if isinstance(content, list):
-                            content_str = ', '.join(f'text: {item['text']}' for item in content if 'text' in item)
+                            content_str = ", ".join(f'text: {item["text"]}' for item in content if "text" in item)
                             formatted_messages.append(f'{role}: {content_str}')
                         else:
                             formatted_messages.append(f'{role}: {content}')
