e28937131559d6a26ebe37024f97b46cf3afd730
patcher9 <patcher@openlit.io>
2025-03-04 14:26:24 +0530

    pylint

--- sdk/python/src/openlit/instrumentation/vertexai/async_vertexai.py
+++ sdk/python/src/openlit/instrumentation/vertexai/async_vertexai.py
@@ -9,7 +9,6 @@ from opentelemetry.sdk.resources import SERVICE_NAME, TELEMETRY_SDK_NAME, DEPLOY
 from openlit.__helpers import (
     get_chat_model_cost,
     handle_exception,
-    response_as_dict,
     calculate_ttft,
     calculate_tbt,
     create_metrics_attributes,
