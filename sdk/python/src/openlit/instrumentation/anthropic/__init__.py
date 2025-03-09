5d2ce415ee6ebf0c889fb6953251272c6a996d58
patcher9 <patcher@openlit.io>
2025-03-09 12:41:03 +0530

    anthropic ready

--- sdk/python/src/openlit/instrumentation/anthropic/__init__.py
+++ sdk/python/src/openlit/instrumentation/anthropic/__init__.py
@@ -1,4 +1,3 @@
-# pylint: disable=useless-return, bad-staticmethod-argument, disable=duplicate-code
 """Initializer of Auto Instrumentation of Anthropic Functions"""
 
 from typing import Collection
@@ -9,7 +8,7 @@ from wrapt import wrap_function_wrapper
 from openlit.instrumentation.anthropic.anthropic import messages
 from openlit.instrumentation.anthropic.async_anthropic import async_messages
 
-_instruments = ("anthropic >= 0.21.0",)
+_instruments = ('anthropic >= 0.21.0',)
 
 class AnthropicInstrumentor(BaseInstrumentor):
     """
@@ -20,27 +19,28 @@ class AnthropicInstrumentor(BaseInstrumentor):
         return _instruments
 
     def _instrument(self, **kwargs):
-        application_name = kwargs.get("application_name", "default")
-        environment = kwargs.get("environment", "default")
-        tracer = kwargs.get("tracer")
-        metrics = kwargs.get("metrics_dict")
-        pricing_info = kwargs.get("pricing_info", {})
-        capture_message_content = kwargs.get("capture_message_content", False)
-        disable_metrics = kwargs.get("disable_metrics")
-        version = importlib.metadata.version("anthropic")
+        application_name = kwargs.get('application_name', 'default')
+        environment = kwargs.get('environment', 'default')
+        tracer = kwargs.get('tracer')
+        event_provider = kwargs.get('event_provider')
+        metrics = kwargs.get('metrics_dict')
+        pricing_info = kwargs.get('pricing_info', {})
+        capture_message_content = kwargs.get('capture_message_content', False)
+        disable_metrics = kwargs.get('disable_metrics')
+        version = importlib.metadata.version('anthropic')
 
         #sync
         wrap_function_wrapper(
-            "anthropic.resources.messages",  
-            "Messages.create",  
+            'anthropic.resources.messages',  
+            'Messages.create',  
             messages(version, environment, application_name,
-                     tracer, pricing_info, capture_message_content, metrics, disable_metrics),
+                     tracer, event_provider, pricing_info, capture_message_content, metrics, disable_metrics),
         )
 
         #async
         wrap_function_wrapper(
-            "anthropic.resources.messages",  
-            "AsyncMessages.create",  
+            'anthropic.resources.messages',  
+            'AsyncMessages.create',  
             async_messages(version, environment, application_name,
                             tracer, pricing_info, capture_message_content, metrics, disable_metrics),
         )
