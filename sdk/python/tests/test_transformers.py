5d7cf984d33f98c1996b8eb498e1381361350a0c
patcher9 <patcher@openlit.io>
2025-05-05 16:32:57 +0530

    check

---
+++ sdk/python/tests/test_transformers.py
@@ -0,0 +1,32 @@
+# pylint: disable=duplicate-code, no-name-in-module
+"""
+This module contains tests for Text Generation functionality in HuggingFace Transformers library.
+
+Tests cover the usage of the Transformers' pipeline for generating text. 
+These tests validate integration with OpenLIT.
+
+Note: Ensure the environment is properly configured for Transformers and OpenLIT monitoring
+prior to running these tests.
+"""
+
+from transformers import pipeline
+import openlit
+
+# Initialize environment and application name for OpenLIT monitoring
+openlit.init(environment="openlit-testing", application_name="openlit-python-test")
+
+def test_text_trasnformers():
+    """
+    Test text generation capabilities from HuggingFace Transformers library.
+    """
+
+    generator = pipeline(model="Qwen/Qwen2.5-1.5B")
+    response = generator("LLM Observability")
+    assert isinstance(response[0]["generated_text"], str)
+
+    chat = [
+        {"role": "system", "content": "You are an OpenTelemetry AI Observability expert"},
+        {"role": "user", "content": "What is Agent Observability?"}
+    ]
+
+    assert isinstance(response[0]["generated_text"][-1]["content"], str)
