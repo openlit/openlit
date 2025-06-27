ed5461e4085909a371a4b65db39b7ac2c04a21df
Patcher <patcher@openlit.io>
2025-06-27 19:47:29 -0400

    [Feat]: Refactor Anthropic OpenTelemetry Monitoring Instrumentation (#782)

--- sdk/python/src/openlit/instrumentation/anthropic/async_anthropic.py
+++ sdk/python/src/openlit/instrumentation/anthropic/async_anthropic.py
@@ -2,7 +2,6 @@
 Module for monitoring Anthropic API calls.
 """
 
-import logging
 import time
 from opentelemetry.trace import SpanKind
 from openlit.__helpers import (
@@ -16,18 +15,14 @@ from openlit.instrumentation.anthropic.utils import (
 )
 from openlit.semcov import SemanticConvention
 
-# Initialize logger for logging potential issues and operations
-logger = logging.getLogger(__name__)
-
-def async_messages(version, environment, application_name, tracer, event_provider,
-             pricing_info, capture_message_content, metrics, disable_metrics):
+def async_messages(version, environment, application_name, tracer, pricing_info, capture_message_content, metrics, disable_metrics):
     """
-    Generates a telemetry wrapper for GenAI function call
+    Generates a telemetry wrapper for Anthropic AsyncMessages.create calls.
     """
 
     class TracedAsyncStream:
         """
-        Wrapper for streaming responses to collect telemetry.
+        Wrapper for async streaming responses to collect telemetry.
         """
 
         def __init__(
@@ -38,24 +33,21 @@ def async_messages(version, environment, application_name, tracer, event_provide
                 kwargs,
                 server_address,
                 server_port,
-                **args,
             ):
             self.__wrapped__ = wrapped
             self._span = span
             self._span_name = span_name
-            self._llmresponse = ''
-            self._response_id = ''
-            self._response_model = ''
-            self._finish_reason = ''
-            self._input_tokens = ''
-            self._output_tokens = ''
-            self._tool_arguments = ''
-            self._tool_id = ''
-            self._tool_name = ''
+            self._llmresponse = ""
+            self._response_id = ""
+            self._response_model = ""
+            self._finish_reason = ""
+            self._input_tokens = 0
+            self._output_tokens = 0
+            self._tool_arguments = ""
+            self._tool_id = ""
+            self._tool_name = ""
             self._tool_calls = None
-            self._response_role = ''
-
-            self._args = args
+            self._response_role = ""
             self._kwargs = kwargs
             self._start_time = time.time()
             self._end_time = None
@@ -86,34 +78,31 @@ def async_messages(version, environment, application_name, tracer, event_provide
                 return chunk
             except StopAsyncIteration:
                 try:
-                    with tracer.start_as_current_span(self._span_name, kind= SpanKind.CLIENT) as self._span:
+                    with self._span:
                         process_streaming_chat_response(
                             self,
                             pricing_info=pricing_info,
                             environment=environment,
                             application_name=application_name,
                             metrics=metrics,
-                            event_provider=event_provider,
                             capture_message_content=capture_message_content,
                             disable_metrics=disable_metrics,
                             version=version
                         )
-
                 except Exception as e:
                     handle_exception(self._span, e)
-                    logger.error("Error in trace creation: %s", e)
                 raise
 
     async def wrapper(wrapped, instance, args, kwargs):
         """
-        Wraps the GenAI function call.
+        Wraps the Anthropic AsyncMessages.create call.
         """
 
-        streaming = kwargs.get('stream', False)
-        server_address, server_port = set_server_address_and_port(instance, 'api.anthropic.com', 443)
-        request_model = kwargs.get('model', 'claude-3-5-sonnet-latest')
+        streaming = kwargs.get("stream", False)
+        server_address, server_port = set_server_address_and_port(instance, "api.anthropic.com", 443)
+        request_model = kwargs.get("model", "claude-3-5-sonnet-latest")
 
-        span_name = f'{SemanticConvention.GEN_AI_OPERATION_TYPE_CHAT} {request_model}'
+        span_name = f"{SemanticConvention.GEN_AI_OPERATION_TYPE_CHAT} {request_model}"
 
         # pylint: disable=no-else-return
         if streaming:
@@ -126,23 +115,27 @@ def async_messages(version, environment, application_name, tracer, event_provide
             with tracer.start_as_current_span(span_name, kind=SpanKind.CLIENT) as span:
                 start_time = time.time()
                 response = await wrapped(*args, **kwargs)
-                response = process_chat_response(
-                    response=response,
-                    request_model=request_model,
-                    pricing_info=pricing_info,
-                    server_port=server_port,
-                    server_address=server_address,
-                    environment=environment,
-                    application_name=application_name,
-                    metrics=metrics,
-                    event_provider=event_provider,
-                    start_time=start_time,
-                    span=span,
-                    capture_message_content=capture_message_content,
-                    disable_metrics=disable_metrics,
-                    version=version,
-                    **kwargs
-                )
+
+                try:
+                    response = process_chat_response(
+                        response=response,
+                        request_model=request_model,
+                        pricing_info=pricing_info,
+                        server_port=server_port,
+                        server_address=server_address,
+                        environment=environment,
+                        application_name=application_name,
+                        metrics=metrics,
+                        start_time=start_time,
+                        span=span,
+                        capture_message_content=capture_message_content,
+                        disable_metrics=disable_metrics,
+                        version=version,
+                        **kwargs
+                    )
+
+                except Exception as e:
+                    handle_exception(span, e)
 
             return response
 
