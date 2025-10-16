d0d3626845cab5bed03a9651f626bd4993b2014c
Gwenn-LR <95341944+Gwenn-LR@users.noreply.github.com>
2025-10-16 08:59:10 +0200

    [Feat]: Fix un-raised error and add wrapping for Ollama `generate` method (#853)
    Co-authored-by: Gwenn Le Roch <leroch@cervval.com>

--- sdk/python/src/openlit/instrumentation/ollama/ollama.py
+++ sdk/python/src/openlit/instrumentation/ollama/ollama.py
@@ -9,6 +9,8 @@ from openlit.instrumentation.ollama.utils import (
     process_chunk,
     process_chat_response,
     process_streaming_chat_response,
+    process_generate_response,
+    process_streaming_generate_response,
     process_embedding_response,
 )
 from openlit.semcov import SemanticConvention
@@ -134,12 +136,160 @@ def chat(
             with tracer.start_as_current_span(span_name, kind=SpanKind.CLIENT) as span:
                 start_time = time.monotonic()
 
-                try:
-                    response = wrapped(*args, **kwargs)
+                response = wrapped(*args, **kwargs)
 
+                try:
                     response = process_chat_response(
                         response=response,
                         gen_ai_endpoint="ollama.chat",
+                        pricing_info=pricing_info,
+                        server_port=server_port,
+                        server_address=server_address,
+                        environment=environment,
+                        application_name=application_name,
+                        metrics=metrics,
+                        start_time=start_time,
+                        span=span,
+                        capture_message_content=capture_message_content,
+                        disable_metrics=disable_metrics,
+                        version=version,
+                        **kwargs,
+                    )
+
+                except Exception as e:
+                    handle_exception(span, e)
+
+            return response
+
+    return wrapper
+
+
+def generate(
+    version,
+    environment,
+    application_name,
+    tracer,
+    pricing_info,
+    capture_message_content,
+    metrics,
+    disable_metrics,
+):
+    """
+    Generates a telemetry wrapper for Ollama generate function call
+    """
+
+    class TracedSyncStream:
+        """
+        Wrapper for streaming responses to collect telemetry.
+        """
+
+        def __init__(
+            self,
+            wrapped,
+            span,
+            span_name,
+            kwargs,
+            server_address,
+            server_port,
+            args,
+        ):
+            self.__wrapped__ = wrapped
+            self._span = span
+            self._llmresponse = ""
+            self._response_model = ""
+            self._finish_reason = ""
+            self._tools = []
+            self._input_tokens = 0
+            self._output_tokens = 0
+            self._response_role = ""
+            self._span_name = span_name
+            self._args = args
+            self._kwargs = kwargs
+            self._start_time = time.monotonic()
+            self._end_time = None
+            self._timestamps = []
+            self._ttft = 0
+            self._tbt = 0
+            self._server_address = server_address
+            self._server_port = server_port
+
+        def __enter__(self):
+            self.__wrapped__.__enter__()
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.__wrapped__.__exit__(exc_type, exc_value, traceback)
+
+        def __iter__(self):
+            return self
+
+        def __getattr__(self, name):
+            """Delegate attribute access to the wrapped object."""
+            return getattr(self.__wrapped__, name)
+
+        def __next__(self):
+            try:
+                chunk = self.__wrapped__.__next__()
+                process_chunk(self, chunk)
+                return chunk
+            except StopIteration:
+                try:
+                    with tracer.start_as_current_span(
+                        self._span_name, kind=SpanKind.CLIENT
+                    ) as self._span:
+                        process_streaming_generate_response(
+                            self,
+                            pricing_info=pricing_info,
+                            environment=environment,
+                            application_name=application_name,
+                            metrics=metrics,
+                            capture_message_content=capture_message_content,
+                            disable_metrics=disable_metrics,
+                            version=version,
+                        )
+                except Exception as e:
+                    handle_exception(self._span, e)
+
+                raise
+
+    def wrapper(wrapped, instance, args, kwargs):
+        """
+        Wraps the Ollama chat function call.
+        """
+
+        streaming = kwargs.get("stream", False)
+
+        server_address, server_port = set_server_address_and_port(
+            instance, "127.0.0.1", 11434
+        )
+        json_body = kwargs.get("json", {}) or {}
+        request_model = json_body.get("model") or kwargs.get("model")
+
+        span_name = f"{SemanticConvention.GEN_AI_OPERATION_TYPE_TEXT_COMPLETION} {request_model}"
+
+        if streaming:
+            awaited_wrapped = wrapped(*args, **kwargs)
+            span = tracer.start_span(span_name, kind=SpanKind.CLIENT)
+            return TracedSyncStream(
+                awaited_wrapped,
+                span,
+                span_name,
+                kwargs,
+                server_address,
+                server_port,
+                args,
+            )
+
+        else:
+            with tracer.start_as_current_span(span_name, kind=SpanKind.CLIENT) as span:
+                start_time = time.monotonic()
+
+                response = wrapped(*args, **kwargs)
+
+                try:
+                    response = process_generate_response(
+                        response=response,
+                        gen_ai_endpoint="ollama.generate",
                         pricing_info=pricing_info,
                         server_port=server_port,
                         server_address=server_address,
@@ -193,9 +343,9 @@ def embeddings(
         with tracer.start_as_current_span(span_name, kind=SpanKind.CLIENT) as span:
             start_time = time.monotonic()
 
-            try:
-                response = wrapped(*args, **kwargs)
+            response = wrapped(*args, **kwargs)
 
+            try:
                 response = process_embedding_response(
                     response=response,
                     gen_ai_endpoint="ollama.embeddings",
