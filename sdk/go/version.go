5803c0981add33ab4b94c6ce5ceca3512d3519bf
AmanAgarwal041 <agarwal.aman041@gmail.com>
2026-03-25 10:35:51 +0530

    chore: updated packages versions

--- sdk/go/version.go
+++ sdk/go/version.go
@@ -3,4 +3,4 @@ package openlit
 // Version is the current version of the OpenLIT Go SDK.
 // This is the single source of truth — update this when cutting a release,
 // then tag the commit as sdk/go/v<Version> to trigger the release workflow.
-const Version = "0.1.0"
+const Version = "0.2.0"
