10a9388630cf17638d0527b27b5a9525df7a7a9d
AmanAgarwal041 <agarwal.aman041@gmail.com>
2026-05-22 21:44:22 +0530

    feat: fix run build

--- src/client/src/app/(playground)/exceptions/page.tsx
+++ src/client/src/app/(playground)/exceptions/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Page() {
 	redirect("/telemetry?tab=exceptions");
+	return null;
 }
--- src/client/src/app/(playground)/requests/page.tsx
+++ src/client/src/app/(playground)/requests/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Page() {
 	redirect("/telemetry?tab=traces");
+	return null;
 }
